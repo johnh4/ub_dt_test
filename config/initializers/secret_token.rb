c8cb44c9f41531f5d5244b54dce2c857c2008bfa
johnh4 <jehowl4@gmail.com>
2013-07-13 10:24:24 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UbDtTest::Application.config.secret_key_base = 'ba2e205a8f89c389b11c538059959591f3c87c20fe3067905059ec1d4803637954afd7b614f089cfabc55508dfc249db625308fa586df0b68847587cc08a3ed0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UbDtTest::Application.config.session_store :cookie_store, key: '_ub_dt_test_session'
